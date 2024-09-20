18cfefa3a4defb4e79230f57273c08f45f3c829f
unknown <issammahtaj02@gmail.com>
2024-09-20 12:58:16 +0200

    ajouter: l'admin interface

--- main.c
+++ main.c
@@ -5,8 +5,13 @@
 void menu_user();
 void sign_up();
 void sign_in();
+int check_pass(char a[], char user[]);
+void admin_menu();
 
 int countclients = 0;
+int isAdmin = 0;
+char admin_user[]= "admin";
+char admin_passw[]= "Admin.123";
 
 struct clients
 {
@@ -49,12 +54,13 @@ void menu_user()
     } while (choix != 3);
 }
 
-// check password in sign up 
+// check password in sign up
 
-int check_pass(char a[],char user[])
+int check_pass(char a[], char user[])
 {
     int i = 0, maj = 0, min = 0, dig = 0, char_sp = 0, somme = 0;
-    if(strstr(a,user) != NULL) {
+    if (strstr(a, user) != NULL)
+    {
         return 0;
     }
 
@@ -76,18 +82,18 @@ int check_pass(char a[],char user[])
         return 1;
 }
 
-// sign up 
+// sign up
 
 void sign_up()
 {
     struct clients newuser;
-    int count = 0,checker;
+    int count = 0, checker;
     printf("entrez vos informations maintenant \n");
     printf("\nvotre nom: ");
     scanf(" %[^\n]", newuser.nom);
     printf("votre prenom: ");
     scanf(" %[^\n]", newuser.prenom);
-    printf("nom d'utilisateur: ");    
+    printf("nom d'utilisateur: ");
     scanf(" %[^\n]", newuser.identifiant);
     printf("numero de telephone: ");
     scanf(" %[^\n]", newuser.tele);
@@ -103,7 +109,7 @@ void sign_up()
         else
             printf("essayez un autre mot de passe: \n");
         scanf(" %[^\n]", newuser.mot_de_passe);
-        checker = check_pass(newuser.mot_de_passe,newuser.identifiant);
+        checker = check_pass(newuser.mot_de_passe, newuser.identifiant);
         count++;
     } while (checker == 0);
     data[countclients] = newuser;
@@ -112,12 +118,13 @@ void sign_up()
     count = 0;
 }
 
-// sign in 
+// sign in
 
 void sign_in()
 {
     char nom[30], password[30];
     int count, pass = 0;
+    isAdmin=0;
     struct clients login;
     for (count = 0; count < 3; count++)
     {
@@ -125,62 +132,91 @@ void sign_in()
         scanf(" %[^\n]", nom);
         printf("entrez votre mot de passe: ");
         scanf(" %[^\n]", password);
-        for (int i = 0; i<countclients; i++)
+        if (strcmp(nom, admin_user) == 0 && strcmp(password, admin_passw)==0)
         {
-            if (strcmp(data[i].identifiant, nom) == 0 && strcmp(data[i].mot_de_passe, password) == 0)
+            isAdmin=1;
+            pass=1;
+        }
+        else
+        {
+            for (int i = 0; i < countclients; i++)
             {
-                printf("%s,vous etes connecte a votre compte ", data[i].prenom);
-                pass=1;
-                break;
+                if (strcmp(data[i].identifiant, nom) == 0 && strcmp(data[i].mot_de_passe, password) == 0)
+                {
+                    printf("%s,vous etes connecte a votre compte ", data[i].prenom);
+                    pass = 1;
+                    break;
+                }
             }
         }
 
         if (pass != 0)
             break;
-        else{
-                printf("Informations incorrectes, veuillez reessayer\n");
-            }
+        else
+        {
+            printf("Informations incorrectes, veuillez reessayer\n");
+        }
     }
 
-    if (pass==1){
-        // lfonctions after sign in
+    if (pass == 1 && isAdmin ==1)
+    {
+        admin_menu();
     }
-    if (count>=3)
+    if (count >= 3)
         printf("desole, vous avez depasse le nombre maximum de tentatives, reessayez plus tard\n");
-        sleep(10);
-    
+    sleep(10);
 }
 
+//admin interface
 
+void admin_menu(){
+    int choix=0;
+    printf("*****vous etes l'administrateur*****\n\n");
+    do{
+    printf("[1]  Ajouter une reclamation\n");
+    printf("[2]  Afficher la liste des reclamations\n");
+    printf("[3]  Modifier ou supprimer une reclamation\n");
+    printf("[4]  Traiter une reclamation\n");
+    printf("[5]  Rechercher une reclamation\n");
+    printf("[6]  Systeme de priorite\n");
+    printf("[7]  Afficher les reclamations ordonnees par priorite\n");
+    printf("[8]  Se deconnecter\n");
+    printf("votre choix= ");
+    scanf("%d",&choix);
+    switch (choix){
+        case 1:
+            // new_reclam();
+            break;
+        case 2:
+            // affich_reclam();
+            break;
+        case 3:
+            // edit_reclam();
+            break;
+        case 4:
+            //traiter_reclam();
+            break;
+        case 5:
+            //find_reclam();
+            break;
+        case 6:
+            //syst_prior();
+            break;
+        case 7:
+            //affich_byprio();
+            break;
+        case 8:
+            printf("***Vous etes deconnecte***\n\n");
+            break;
+        default:
+        printf("ce choix n'existe pas, essayez-en un autre\n");
+        break;
+    }
+    }while(choix!=8);
 
-// void menu()
-// {
-//     int choix;
-//     printf("voulez-vous : \n");
-//     do
-//     {
-//         printf("(1) Gestion des reclamations\n");
-//         printf("(2) Statistiques et Rapports\n");
-//         printf("(3) quitter le programme\n");
-//         scanf("%d", &choix);
-//         switch (choix)
-//         {
-//         case 1:
-//             // gestion_recl();
-//             break;
-//         case 2:
-//             // stats_rapp();
-//             break;
-//         case 3:
-//             printf("Merci de nous rendre visite");
-//             break;
-//         default:
-//             printf("ce choix n'existe pas, essayez-en un autre\n");
-//             break;
-//         }
-//     } while (choix != 3);
-// }
-
+    if (choix==8)
+        menu_user();
+}
 
 int main()
 {
