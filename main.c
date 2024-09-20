ebc9f189e1e971fae5feddbe102389db0079ce7e
unknown <issammahtaj02@gmail.com>
2024-09-20 11:14:08 +0200

    Correction de bugs :crer des comptes utilisateurs

--- main.c
+++ main.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
 void menu_user();
-void stats_rapp();
 void sign_up();
 void sign_in();
-int check_pass();
 
 int countclients = 0;
 
@@ -20,121 +19,12 @@ struct clients
 
 struct clients data[100];
 
-void menu()
-{
-    int choix;
-    printf("voulez-vous : \n");
-    do
-    {
-        printf("(1) Gestion des reclamations\n");
-        printf("(2) Statistiques et Rapports\n");
-        printf("(3) quitter le programme\n");
-        scanf("%d", &choix);
-        switch (choix)
-        {
-        case 1:
-            // gestion_recl();
-            break;
-        case 2:
-            // stats_rapp();
-            break;
-        case 3:
-            printf("Merci de nous rendre visite");
-            break;
-        default:
-            printf("ce choix n'existe pas, essayez-en un autre\n");
-            break;
-        }
-    } while (choix != 3);
-}
-
-int check_pass(char a[])
-{
-    int i = 0, maj = 0, min = 0, dig = 0, char_sp = 0, somme = 0;
-    for (i = 0; a[i] != '\0'; i++)
-    {
-        if (a[i] >= 'A' && a[i] <= 'Z')
-            maj = 1;
-        if (a[i] >= 'a' && a[i] <= 'z')
-            min = 1;
-        if (a[i] >= '0' && a[i] <= '9')
-            dig = 1;
-        if (a[i] >= 33 && a[i] <= 47 || a[i] >= 58 && a[i] <= 64 || a[i] >= 91 && a[i] <= 96 || a[i] >= 123 && a[i] <= 126)
-            char_sp = 1;
-    }
-    somme = maj + min + dig + char_sp;
-    if (i < 8 || somme != 4)
-        return 0;
-    else
-        return 1;
-}
-
-void sign_up()
-{
-    struct clients newuser;
-    int count = 0, checker;
-    char checker2;
-    printf("entrez vos informations maintenant \n");
-    printf("\nvotre nom: ");
-    scanf(" %[^\n]", newuser.nom);
-    printf("votre prenom: ");
-    scanf(" %[^\n]", newuser.prenom);
-    printf("nom d'utilisateur: ");
-    scanf(" %[^\n]", newuser.identifiant);
-    printf("numero de telephone: ");
-    scanf(" %[^\n]", newuser.tele);
-    do
-    {
-        if (count == 0)
-            printf("mot de passe: ");
-        else if (count == 1)
-        {
-            printf("essayez un autre mot de passe(Doit contenir au moins : Une lettre majuscule / Une lettre minuscule / Un chiffre / Un caractere special)");
-            printf("8 caracteres minimale,Ne doit pas contenir le nom d'utilisateur\n");
-        }
-        else
-            printf("essayez un autre mot de passe: \n");
-        scanf(" %[^\n]", newuser.mot_de_passe);
-        checker = check_pass(newuser.mot_de_passe);
-        count++;
-    } while (checker == 0);
-    data[countclients] = newuser;
-    countclients++;
-    printf("vous avez cree un compte avec succes\n");
-    count = 0;
-}
-
-void sign_in()
-{
-    char nom[30], password[30];
-    int count, pass = 0;
-    struct clients login;
-    for (count = 0; count <= 3; count++)
-    {
-        printf("entrez votre nom d'utilisateur: ");
-        scanf(" %[^\n]", nom);
-        printf("entrez votre mot de passe: ");
-        scanf(" %[^\n]", password);
-        for (int i = 0; data[i].identifiant != '\0'; i++)
-        {
-            if (strcmp(data[i].identifiant, nom) == 0 && strcmp(data[i].mot_de_passe, password) == 0)
-            {
-                printf("%s,vous etes connecte a votre compte ", data[i].prenom);
-                pass++;
-                break;
-                //the error is here
-            }
-        }
-        if (pass != 0)
-            break;
-    }
-    menu();
-}
+// first page (menu)
 
 void menu_user()
 {
     int choix;
-    printf("bienvenue sur notre site de gerer les reclamations de notre clients\n");
+    printf("\nbienvenue sur notre site de gerer les reclamations de notre clients\n");
     printf("voulez-vous : \n");
     do
     {
@@ -158,6 +48,139 @@ void menu_user()
         }
     } while (choix != 3);
 }
+
+// check password in sign up 
+
+int check_pass(char a[],char user[])
+{
+    int i = 0, maj = 0, min = 0, dig = 0, char_sp = 0, somme = 0;
+    if(strstr(a,user) != NULL) {
+        return 0;
+    }
+
+    for (i = 0; a[i] != '\0'; i++)
+    {
+        if (a[i] >= 'A' && a[i] <= 'Z')
+            maj = 1;
+        if (a[i] >= 'a' && a[i] <= 'z')
+            min = 1;
+        if (a[i] >= '0' && a[i] <= '9')
+            dig = 1;
+        if (a[i] >= 33 && a[i] <= 47 || a[i] >= 58 && a[i] <= 64 || a[i] >= 91 && a[i] <= 96 || a[i] >= 123 && a[i] <= 126)
+            char_sp = 1;
+    }
+    somme = maj + min + dig + char_sp;
+    if (i < 8 || somme != 4)
+        return 0;
+    else
+        return 1;
+}
+
+// sign up 
+
+void sign_up()
+{
+    struct clients newuser;
+    int count = 0,checker;
+    printf("entrez vos informations maintenant \n");
+    printf("\nvotre nom: ");
+    scanf(" %[^\n]", newuser.nom);
+    printf("votre prenom: ");
+    scanf(" %[^\n]", newuser.prenom);
+    printf("nom d'utilisateur: ");    
+    scanf(" %[^\n]", newuser.identifiant);
+    printf("numero de telephone: ");
+    scanf(" %[^\n]", newuser.tele);
+    do
+    {
+        if (count == 0)
+            printf("mot de passe: ");
+        else if (count == 1)
+        {
+            printf("essayez un autre mot de passe(Doit contenir au moins : Une lettre majuscule / Une lettre minuscule / Un chiffre / Un caractere special)");
+            printf("8 caracteres minimale,Ne doit pas contenir le nom d'utilisateur\n");
+        }
+        else
+            printf("essayez un autre mot de passe: \n");
+        scanf(" %[^\n]", newuser.mot_de_passe);
+        checker = check_pass(newuser.mot_de_passe,newuser.identifiant);
+        count++;
+    } while (checker == 0);
+    data[countclients] = newuser;
+    countclients++;
+    printf("vous avez cree un compte avec succes\n");
+    count = 0;
+}
+
+// sign in 
+
+void sign_in()
+{
+    char nom[30], password[30];
+    int count, pass = 0;
+    struct clients login;
+    for (count = 0; count < 3; count++)
+    {
+        printf("entrez votre nom d'utilisateur: ");
+        scanf(" %[^\n]", nom);
+        printf("entrez votre mot de passe: ");
+        scanf(" %[^\n]", password);
+        for (int i = 0; i<countclients; i++)
+        {
+            if (strcmp(data[i].identifiant, nom) == 0 && strcmp(data[i].mot_de_passe, password) == 0)
+            {
+                printf("%s,vous etes connecte a votre compte ", data[i].prenom);
+                pass=1;
+                break;
+            }
+        }
+
+        if (pass != 0)
+            break;
+        else{
+                printf("Informations incorrectes, veuillez reessayer\n");
+            }
+    }
+
+    if (pass==1){
+        // lfonctions after sign in
+    }
+    if (count>=3)
+        printf("desole, vous avez depasse le nombre maximum de tentatives, reessayez plus tard\n");
+        sleep(10);
+    
+}
+
+
+
+// void menu()
+// {
+//     int choix;
+//     printf("voulez-vous : \n");
+//     do
+//     {
+//         printf("(1) Gestion des reclamations\n");
+//         printf("(2) Statistiques et Rapports\n");
+//         printf("(3) quitter le programme\n");
+//         scanf("%d", &choix);
+//         switch (choix)
+//         {
+//         case 1:
+//             // gestion_recl();
+//             break;
+//         case 2:
+//             // stats_rapp();
+//             break;
+//         case 3:
+//             printf("Merci de nous rendre visite");
+//             break;
+//         default:
+//             printf("ce choix n'existe pas, essayez-en un autre\n");
+//             break;
+//         }
+//     } while (choix != 3);
+// }
+
 
 int main()
 {
